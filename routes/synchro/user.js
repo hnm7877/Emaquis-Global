19ca0e6e9045e2020a5fc8d48cd94e3eab819083
Rolls98 <toureb16@gmail.com>
2024-02-05 14:53:13 +0000

    get user with employees

--- routes/synchro/user.js
+++ routes/synchro/user.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const { authSynchroApi } = require('../../middleware/auth');
 const userModel = require('../../models/user.model');
+const employeModel = require('../../models/employe.model');
 const router = express.Router();
 
 router.get('', authSynchroApi, async (req, res) => {
 	const users = await userModel.find();
+
+	const usersWithEmployees = await Promise.all(
+		users.map(async (user) => {
+			const employees = await employeModel.find({
+				travail_pour: user._id,
+			});
+
+			return {
+				...user._doc,
+				employees,
+			};
+		})
+	);
+
 	res.send({
 		success: true,
-		data: users,
+		data: usersWithEmployees,
 	});
 });
 
